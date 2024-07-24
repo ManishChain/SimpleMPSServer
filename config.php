725570b72d7ee81970c6408e733970a1e8abfe15
ManishChain <61581454+ManishChain@users.noreply.github.com>
2024-07-24 15:20:27 +0530

    Adding initial files

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+ 
+/* Define MySQL connection details and database table name 
+User “myperzem_simplemps” was added to the database “myperzem_simplemps”.
+*/ 
+
+$SETTINGS["hostname"] = 'localhost:3306' ;
+$SETTINGS["mysql_user"] = 'myperzem_simplemps';
+$SETTINGS["mysql_pass"] = 'zxcv1234@MS$';
+$SETTINGS["mysql_database"] = 'myperzem_simplemps';
+
+$connection = mysqli_connect($SETTINGS["hostname"], $SETTINGS["mysql_user"], $SETTINGS["mysql_pass"], $SETTINGS["mysql_database"]) or die ('Unable to connect to MySQL server.<br ><br >Please make sure your MySQL login details are correct.');
+
+?>
---
+++ message.php
@@ -0,0 +1,32 @@
+<?php
+try {
+    include("config.php");
+    
+    $_POST = json_decode(file_get_contents('php://input'), true);
+    
+    $owner = $_POST ["owner"] ;	
+    $device_id = $_POST ["device_id"] ;
+    $message = $_POST ["message"] ;
+    $spy = $_POST ["spy"] ;	
+    $testing = $_POST ["testing"] ;
+    $active = $_POST ["active"] ;
+    $battery = $_POST ["battery"] ;
+    
+    $query = "INSERT INTO `MESSAGE`(`OWNER`, `DEVICE_ID`, `MESSAGE`, `SPY`, `TESTING`, `ACTIVE`, `BATTERY`) VALUES ( '$owner', $device_id, '$message', $spy, $testing, $active, $battery ) " ;
+    mysqli_query ($connection, $query) or die ('request "Could not execute SQL query" '.$query);
+
+} catch (ExceptionType $e) {
+    
+} finally {
+    $connection->close();
+}
+
+?>
+
+<?php
+$jsonData = array(
+    'status' => 'ok'
+);
+header('Content-Type: application/json');
+echo json_encode($jsonData);
+?>
